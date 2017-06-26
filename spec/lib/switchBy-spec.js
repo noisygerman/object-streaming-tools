862eeda7a4bebe2d8a0a48a84e43fc42aada10cf
Markus Westerholz <mwesterholz@gmail.com>
2017-06-25 23:31:32 -0700

    add objects-as-key test

--- spec/lib/switchBy-spec.js
+++ spec/lib/switchBy-spec.js
@@ -1,5 +1,8 @@
 describe( 'Instances of switchBy streams', ()=>{
 
+  const assert
+    = require( 'assert' );
+  
   const just
     = require( '../../lib/just' );
 
@@ -29,7 +32,7 @@ describe( 'Instances of switchBy streams', ()=>{
 
     }
 
-    function onKeyMatch( _, next ){
+    function thenDo( _, next ){
       
       next( null, key );
      
@@ -37,7 +40,7 @@ describe( 'Instances of switchBy streams', ()=>{
   
     just( input )
       .on( 'error', onError )
-      .pipe( switchBy( predicate, [ { ifMatches: key, thenDo: onKeyMatch } ] ) )
+      .pipe( switchBy( predicate, [ { ifMatches: key, thenDo } ] ) )
       .on( 'error', onError )
       .on( 'finish', ()=>{
 
@@ -49,5 +52,136 @@ describe( 'Instances of switchBy streams', ()=>{
       } );
 
   } );
+
+  it( 'should call matching functions with the original value', ( done )=>{
+
+    const input
+      = 'any';
+      
+    const key
+      = 'some key';
+    
+    let actual;
+
+    function predicate( val, next ){
+
+      next( null, key ); 
+
+    }
+
+    function thenDo( val, next ){
+      
+      actual = val;
+      next( null, key );
+     
+    }
+  
+    just( input )
+      .on( 'error', onError )
+      .pipe( switchBy( predicate, [ { ifMatches: key, thenDo } ] ) )
+      .on( 'error', onError )
+      .on( 'finish', ()=>{
+
+        expect( actual )
+          .toBe( input );
+        
+        done();
+
+      } );
+
+  } );
+
+  it( 'should return the result of the matched function', ( done )=>{
+
+    const input
+      = 'any';
+
+    const modifier
+      = 'old string';
+
+    const key
+      = 'some key';
+    
+    let expected;
+
+    function predicate( val, next ){
+
+      next( null, key ); 
+
+    }
+
+    function thenDo( val, next ){
+
+      assert( expected == null );
+
+      expected = `${ val } ${ modifier }`;
+      next( null, expected );
+     
+    }
+  
+    let actual;
+
+    just( input )
+      .on( 'error', onError )
+      .pipe( switchBy( predicate, [ { ifMatches: key, thenDo } ] ) )
+      .on( 'error', onError )
+      .on( 'data', ( result )=>{
+
+        assert( actual == null );
+        actual = result;
+
+      } )
+      .on( 'finish', ()=>{
+
+        expect( actual )
+          .toBe( expected );
+        
+        done();
+
+      } );
+
+  } );
+
+  it( 'should allow for objects as keys', ( done )=>{
+
+    const input
+      = 'any';
+      
+    const key
+      = 'some key';
+      
+    const keyObj
+      = { key };
+
+    let wasCalled;
+
+    function predicate( val, next ){
+
+      next( null, keyObj ); 
+
+    }
+
+    function thenDo( val, next ){
+      
+      wasCalled = true;
+      next();
+     
+    }
+  
+    just( input )
+      .on( 'error', onError )
+      .pipe( switchBy( predicate, [ { ifMatches: keyObj, thenDo } ] ) )
+      .on( 'error', onError )
+      .on( 'finish', ()=>{
+
+        expect( wasCalled )
+          .toBe( true );
+        
+        done();
+
+      } );
+
+  } );
+
 
 } );
